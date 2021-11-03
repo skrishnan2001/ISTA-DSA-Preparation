114b94c4352985d42499cd94bfc5fc8f8f2f7e29
Krishnan S <skrishnan2001@gmail.com>
2021-11-03 15:37:33 +0530

    Implemented KthSmallestElement using priority queues

--- Arrays/KthSamllestElement.java
+++ Arrays/KthSamllestElement.java
@@ -2,12 +2,24 @@ package Arrays;
 
 import java.util.*;
 
-//TC: O(nlogn); SC: O(1)
+//TC: O(nlogk); SC: O(k)
 
 public class KthSamllestElement {
     public static int kthSmallest(int[] arr, int l, int r, int k) 
     { 
-        Arrays.sort(arr);
-        return arr[k - 1];
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
+        
+        for(int i = l; i < k; i++)
+            maxHeap.add(arr[i]);
+            
+        for(int i = k; i <= r; i++)
+        {
+            if(arr[i] < maxHeap.peek())
+            {
+                maxHeap.poll();
+                maxHeap.add(arr[i]);
+            }
+        }
+        return maxHeap.peek();
     } 
 }
